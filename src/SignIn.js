4b01b45c19015da9d6ad6f9b04980f52277974db
Aleksei2576 <118676730+Aleksei2576@users.noreply.github.com>
2023-07-17 21:41:08 +0300

    Update SignIn.js

--- src/SignIn.js
+++ src/SignIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
 
 	onSubmit = (e) => {
 		e.preventDefault();
-		fetch('https://smart-brain-bmca.onrender.com/signin', {
+		fetch('https://smart-brain-qqks.onrender.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
