0b2388aef2c26b17e443bf057c41ee32fe225493
Aleksei2576 <118676730+Aleksei2576@users.noreply.github.com>
2023-07-20 09:26:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ loadUser = (data) => {
       })
     })
     .then(response => response.json())
-    .then(data => this.calculateBox(data))
+    .then(data => console.log(data))
   }
       
   signinChange = (route) => {
